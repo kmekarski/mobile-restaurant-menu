fed3e8afe093159cff65a3514f0f7e92dd813b87
kmekarski <72306134+kmekarski@users.noreply.github.com>
2023-03-15 10:21:20 +0100

    v2

--- index.js
+++ index.js
@@ -1,49 +1,74 @@
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
 import { menuArray } from './data.js'
 
 const menuEl = document.querySelector('.menu')
-const orderSummaryEl = document.querySelector('.order-summary')
 const orderedItemsEl = document.querySelector('.ordered-items')
 const totalPriceEl = document.getElementById('total-price')
-const orderBtn = document.getElementById('order-btn')
+
+const orderSummaryEl = document.querySelector('.order-summary')
+const paymentModalEl = document.querySelector('.payment-modal')
+const confirmEl = document.querySelector('.confirm-container')
+
+const modalForm = document.querySelector('.modal-form')
+
 
 let orderArray = []
-let totalPrice = 0
-
 
 document.addEventListener('click', (e) => {
     if(e.target.dataset.addBtn) {
         handleAddBtnClick(e.target.dataset.addBtn)
     }
+    if(e.target.dataset.remove) {
+        handleRemoveItemClick(e.target.dataset.remove)
+    }
+    if(e.target.id === 'order-btn') {
+        handleOrderBtnClick()
+    }
+})
+
+modalForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    orderArray = []
+    paymentModalEl.classList.add('hidden')
+    orderSummaryEl.classList.add('hidden')
+    confirmEl.classList.remove('hidden')
+    render()
 })
 
 function handleAddBtnClick(itemId) {
 
-    let clickedItemName = menuArray.filter( (item) => { return item.id == itemId })[0].name
-
-    orderArray.push(clickedItemName)
-    // console.log(orderArray)
-
+    const clickedItemObj = menuArray.filter( (item) => { return item.id == itemId })[0]
+    const orderedItem = {
+        name: clickedItemObj.name,
+        price: clickedItemObj.price,
+        id: uuidv4()
+    }
+    orderArray.push(orderedItem)
+    orderSummaryEl.classList.remove('hidden')
+    confirmEl.classList.add('hidden')
     render()
+}
+function handleRemoveItemClick(itemId) {
+    orderArray = orderArray.filter( (item) => { return item.id !== itemId })
+    if(!orderArray.length) {
+        orderSummaryEl.classList.add('hidden')
+    }
+    render()
+}
 
-
+function handleOrderBtnClick() {
+    paymentModalEl.classList.remove('hidden')
 }
 
 function getMenu() {
     let menuStr = ''
     menuArray.forEach( (item) => {
-        let ingredientsStr = ''
-        for(let i=0; i<item.ingredients.length; i++) {
-            ingredientsStr += item.ingredients[i]
-            if(i !== item.ingredients.length - 1) {
-                ingredientsStr += ','
-            }
-        }
         menuStr += `
                 <div class="menu-item">
                     <p class="item-emoji">${item.emoji}</p>
                     <div class="item-text">
                         <p class="item-title regular-text">${item.name}</p>
-                        <p class="item-desc small-text">${ingredientsStr}</p>
+                        <p class="item-desc small-text">${getIngredientsString(item.ingredients)}</p>
                         <p class="item-price price-text">$${item.price}</p>
                     </div>
                     <div class="add-btn data-add-btn="${item.id}"">
@@ -55,43 +80,54 @@ function getMenu() {
     return menuStr
 }
 
+function getIngredientsString(ingredientsArray) {
+    let ingredientsStr = ''
+    for(let i=0; i<ingredientsArray.length; i++) {
+        ingredientsStr += ingredientsArray[i]
+        if(i !== ingredientsArray.length - 1) {
+            ingredientsStr += ','
+        }
+    }
+    return ingredientsStr;
+}
+
 function getOrderedItems() {
     let orderedItemsStr = ''
-
-
     orderArray.forEach( (item) => {
-        const menuItemObj = menuArray.filter( (menuItem) => {
-            return menuItem.name == item
-        })[0]
-        console.log(menuItemObj.name)
         orderedItemsStr += `
                      <div class="ordered-item">
-                        <p class="regular-text">${item}</p>
-                        <p class="remove-text">remove</p>
-                        <p class="price-right price-text">$${menuItemObj.price}</p>
+                        <p class="regular-text">${item.name}</p>
+                        <p class="remove-text" data-remove="${item.id}">remove</p>
+                        <p class="price-right price-text">$${item.price}</p>
                     </div>
         `
     })
-
     return orderedItemsStr
 }
 
 function getTotalPrice() {
     let totalPrice = 0
     orderArray.forEach( (item) => {
-        const menuItemObj = menuArray.filter( (menuItem) => {
-            return menuItem.name == item
-        })[0]
-        totalPrice += menuItemObj.price
+        totalPrice += item.price
     })
     return totalPrice
 }
 
+function getConfirmText() {
+    const username = document.getElementById('username').value
+    const confirmText = `
+                <div class="confirm">
+                    <p class="confirm-text">Thanks, ${username}! Your order is on its way!</p>
+                </div>
+                `
+    return confirmText
+}
+
 function render() {
     menuEl.innerHTML = getMenu()
-    orderSummaryEl.classList.remove('hidden')
     orderedItemsEl.innerHTML = getOrderedItems()
     totalPriceEl.textContent = '$' + getTotalPrice()
+    confirmEl.innerHTML = getConfirmText()
 }
 
 render()
